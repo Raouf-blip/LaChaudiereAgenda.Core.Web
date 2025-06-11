73de8332bd6ba9d15a708f94517a22d2a1dc93f1
ClemDW <clementdewasch54@gmail.com>
2025-06-11 11:55:29 +0200

    fix: db

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,4 +1,7 @@
-CREATE DATABASE IF NOT EXISTS lachaudiere;
+CREATE DATABASE IF NOT EXISTS lachaudiere
+  DEFAULT CHARACTER SET utf8mb4
+  COLLATE utf8mb4_general_ci;
+
 USE lachaudiere;
 
 -- Table des utilisateurs
